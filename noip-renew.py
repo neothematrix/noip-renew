6d546537de2ac1594a154eb6ba55214d2d447f5e
Gabriele <neothematrix@users.noreply.github.com>
2024-05-04 16:25:47 +0200

    added parameter sanity check for docker execution

--- noip-renew.py
+++ noip-renew.py
@@ -242,10 +242,11 @@ def main(argv=None):
     DOCKER = os.environ.get("CONTAINER", "").lower() in ("yes", "y", "on", "true", "1")
     if DOCKER:
         print("Running inside docker container")
-        noip_username = os.environ.get('NOIP_USERNAME')
-        noip_password = os.environ.get('NOIP_PASSWORD')
-        noip_totp = os.environ.get('NOIP_2FA_SECRET_KEY')
+        noip_username = os.environ.get('NOIP_USERNAME','')
+        noip_password = os.environ.get('NOIP_PASSWORD','')
+        noip_totp = os.environ.get('NOIP_2FA_SECRET_KEY','')
         debug = int(os.environ.get('NOIP_DEBUG', 1))
+        if len(noip_username) == 0 or len(noip_password) == 0 or len(noip_totp) == 0: sys.exit('You are using docker, you need to specify the required parameters as environment varialbes, check the documentation.')
     else:
         noip_username, noip_password, noip_totp, debug = get_args_values(argv)
     return (Robot(noip_username, noip_password, noip_totp, debug, DOCKER)).run()
